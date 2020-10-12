9743d8296eb63ea3cb26e54dbced6a9b488b9fc5
itallcanw8 <mironchik.a@gmail.com>
2020-10-12 22:55:25 +0300

    first lab vers. 2

--- 1/Source.cpp
+++ 1/Source.cpp
@@ -1,22 +1,26 @@
 ﻿#include <iostream>
 
-void mmx() 
+void mmx(_int8 v_A[], _int8 v_B[], _int8 v_C[], _int16 v_D[])
 {
-	_int8 A[8] = { 7,6,5,4, 3,2, 1,0 };
-	_int8 B[8] = { 1,1,1,0, 1,0, 1,1 };
-	_int8 C[8] = { 3,4,5,10,7,10,9,10 };
-	_int16 D[8] = { 10,10,10,10,10,10,10,10 };
+	_int8  A[8] = { };
+	_int8  B[8] = { };
+	_int8  C[8] = { };
+	_int16 D[8] = { };
+	_int16 F[8] = { };
 
-	_int16 F[8] = {};
-
-	_asm 
+	for (int i = 0; i < 8; i++)
 	{
-		movq mm0, qword ptr[A]
-		movq mm1, qword ptr[B]
-		movq mm2, qword ptr[C]
-		movq mm3, qword ptr[D]
+		A[i] = v_A[i]; B[i] = v_B[i]; C[i] = v_C[i]; D[i] = v_D[i];
+	}
 
-		punpcklbw mm0, mm0 //int8 -> int16
+	_asm
+	{
+		movq mm0, [A]
+		movq mm1, [B]
+		movq mm2, [C]
+		movq mm3, [D]
+
+		punpcklbw mm0, mm0 //int8 -> int16 
 		punpcklbw mm1, mm1
 		punpcklbw mm2, mm2
 
@@ -30,37 +34,48 @@ void mmx()
 
 		movq  qword ptr[F], mm0
 
-		movq mm0, qword ptr[A + 4]
-		movq mm1, qword ptr[B + 4]
-		movq mm2, qword ptr[C + 4]
+		movq mm0, [A + 4]
+		movq mm1, [B + 4]
+		movq mm2, [C + 4]
 		movq mm3, [D + 8]
 
 		punpcklbw mm0, mm0
 		punpcklbw mm1, mm1
 		punpcklbw mm2, mm2
 
-		psraw     mm0, 8
-		psraw     mm1, 8
-		psraw     mm2, 8
+		psraw mm0, 8
+		psraw mm1, 8
+		psraw mm2, 8
 
 		pmullw mm0, mm1 //A*B (sec half)
 		paddsw mm0, mm2 //A*B+C (sec half)
 		psubsw mm0, mm3 //A*B+C-D (sec half)
 
 		movq  qword ptr[F + 8], mm0
-		
+
 		emms
 	}
 
+	std::cout << "mmx: ";
+
 	for (int i = 0; i < 8; i++)
-	{
-		std::cout << F[i]<<" ";
-	}
+		std::cout << F[i] << " ";
+
+	std::cout << "\nc++: ";
+
+	for (int i = 0; i < 8; i++)
+		std::cout << A[i] * B[i] + C[i] - D[i] << " ";
+
 }
 
 int main()
 {
-	mmx();
+	_int8  A[8] = { 7,6,5,4, 3,2, 1,0 };
+	_int8  B[8] = { 1,1,1,0, 1,0, 1,1 };
+	_int8  C[8] = { 3,4,5,10,7,10,9,10 };
+	_int16 D[8] = { 11,10,10,10,10,10,10,10 };
+
+	mmx(A, B, C, D);
 
 	system("pause");
 	return 0;
